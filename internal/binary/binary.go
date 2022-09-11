4de542c6100c91fa807bb01cdd5b592e59b86614
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-09-10 23:41:40 -0300

    feat: add web request

--- internal/binary/binary.go
+++ internal/binary/binary.go
@@ -1,6 +1,31 @@
 package binary
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+)
+
+func WebRequest() {
+	resp, err := http.Get("http://localhost:9000/api/tasks")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// Print the HTTP response status.
+	fmt.Println("Response status:", resp.Status)
+
+	// Print the first 5 lines of the response body.
+	scanner := bufio.NewScanner(resp.Body)
+	for i := 0; scanner.Scan() && i < 5; i++ {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+}
 
 func Create() {
 	fmt.Println("Created Task")
@@ -8,6 +33,7 @@ func Create() {
 
 func List() {
 	fmt.Println("Listed Tasks")
+	WebRequest()
 }
 
 func Update() {
