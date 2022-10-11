9a80c66de2b7415a4535f8d31c061a77578d35ff
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-10-10 23:43:13 -0300

    feat: add mocks

--- entities/task_service_test.go
+++ entities/task_service_test.go
@@ -1,1 +1,25 @@
 package entities
+
+import (
+	"github.com/golang/mock/gomock"
+	mock_entities "github.com/tonnytg/tasklist/entities/mocks"
+	"testing"
+)
+
+func TestTaskService_Get(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	task := mock_entities.NewMockTaskInterface(ctrl)
+	persistence := mock_entities.NewMockTaskPersistenceInterface(ctrl)
+	persistence.EXPECT().Get(gomock.Any()).Return(task, nil).AnyTimes()
+
+	service := TaskService{Persistence: persistence}
+	result, err := service.Get(1)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	if result != task {
+		t.Errorf("Expected %v, got %v", task, result)
+	}
+}
