c562e95a27a0829eab7a6539a26e9c954d50d70b
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-10-25 08:21:52 -0300

    feat: add error handling tasks return

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"github.com/tonnytg/tasklist/entities"
 	"gorm.io/driver/sqlite"
@@ -18,18 +19,18 @@ func init() {
 	db.AutoMigrate(&entities.Task{})
 }
 
-func CreateTask(task entities.Task) (entities.Task, error) {
+func CreateTask(task entities.Task) (*entities.Task, error) {
 
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
-		panic("func create failed to connect database")
+		return nil, err
 	}
 
 	tx := db.Create(&task)
-	return task, tx.Error
+	return &task, tx.Error
 }
 
-func GetTask(ID uint16) entities.Task {
+func GetTask(ID uint16) (*entities.Task, error) {
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		panic("func create failed to connect database")
@@ -37,43 +38,57 @@ func GetTask(ID uint16) entities.Task {
 
 	tempTask := entities.Task{}
 	db.First(&tempTask, ID)
-	fmt.Println("Achei", tempTask)
-	return tempTask
+	if tempTask.ID == 0 {
+		return nil, errors.New("task not found")
+	}
+	return &tempTask, nil
 }
 
-func ListTask() []entities.Task {
+func ListTask() ([]entities.Task, error) {
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
-		panic("func create failed to connect database")
+		return nil, err
 	}
 
 	var tempTask []entities.Task
 	db.Find(&tempTask)
-	return tempTask
+	return tempTask, nil
 }
 
-func UpdateTask(ID uint16, name string, description string) {
+func UpdateTask(ID uint16, name string, description string) (*entities.Task, error) {
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		panic("func create failed to connect database")
 	}
 
-	task := GetTask(ID)
+	task, err := GetTask(ID)
+	if err != nil {
+		return nil, err
+	}
 	task.Name = name
 	task.Description = description
 	db.Save(&task)
+
+	return task, nil
 }
 
-func DeleteAllTasks() {
+func DeleteAllTasks() error {
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
-		panic("func create failed to connect database")
+		return fmt.Errorf("failed to connect database: %s", err)
 	}
 
 	db.Where("status = ?", entities.DONE).Delete(&entities.Task{})
 	db.Where("status = ?", entities.CANCELED).Delete(&entities.Task{})
 	db.Where("status = ?", entities.DOING).Delete(&entities.Task{})
 	db.Where("status = ?", entities.BACKLOG).Delete(&entities.Task{})
+
+	tasks := []entities.Task{}
+	db.Find(&tasks)
+	if len(tasks) != 0 {
+		return errors.New("failed to delete all tasks")
+	}
+	return nil
 }
 
 func DeleteTask(task entities.Task) {
