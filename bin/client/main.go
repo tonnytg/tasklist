57a06943d3c224ff7bb20f5af0f78f052a2ad969
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-11-05 21:51:14 -0300

    feat: add new commands for client

--- bin/client/main.go
+++ bin/client/main.go
@@ -1,31 +1,42 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 )
 
-const ClientHelp string = `
-Help Client!`
+const ClientHelp string = `pass help as argument to get help.
+./taskclient help`
+const AddHelp string = `Help Add!`
 
 func main() {
 	fmt.Println("Client Side")
 
 	if len(os.Args) < 2 {
 		fmt.Println(ClientHelp)
-		fmt.Println("Example: ./script --option api")
 		os.Exit(0)
 	}
 
-	//add := flag.String("add", "", "--add <VALUE>")
-	//description := flag.String("description", "", "--description <VALUE>")
-	//list := flag.Bool("list", false, "--list")
-	help := flag.Bool("help", false, "--help")
-
-	flag.Parse()
-
-	if *help != false {
+	os.Args = os.Args[1:]
+	if os.Args[0] == "help" {
 		fmt.Println(ClientHelp)
+		os.Exit(0)
 	}
+
+	if os.Args[0] == "add" {
+		if len(os.Args) < 2 {
+			fmt.Println(AddHelp)
+			os.Exit(1)
+		}
+		fmt.Println("Add")
+		args := os.Args[1:]
+		fmt.Println("lenArgs:", len(args))
+		fmt.Println("Title:", args[0])
+		if args[1] == "desc" || args[1] == "description" {
+			fmt.Println("Description:", args[2])
+		}
+		os.Exit(0)
+	}
+	fmt.Println("args not found")
+	os.Exit(1)
 }
